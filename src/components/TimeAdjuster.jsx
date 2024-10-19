e35c5a3c52325aab1544d955bc7e756d6424c28c
Marcelo-G21 <marcelo.garcia.arenas@gmail.com>
2024-10-19 20:52:50 -0300

    feat: handle added in TimeAdjuster

--- src/components/TimeAdjuster.jsx
+++ src/components/TimeAdjuster.jsx
@@ -1,20 +1,22 @@
 import AdjustButton from './AdjustButton';
 
-const TimeAdjuster = ({ workTime,
-    breakTime,
-    adjustTime,
-    isActive, }) => {
+const TimeAdjuster = ({ workTime, breakTime, adjustTime, isActive }) => {
+  const handleAdjustTime = (amount, isSession) => {
+    if (isSession && workTime <= 1 && amount < 0) return; // Evita que session time baje de 1
+    if (!isSession && breakTime <= 1 && amount < 0) return; // Evita que break time baje de 1
+    adjustTime(amount, isSession); // Llama a la función original si las condiciones son correctas
+  };
   return (
     <div className="flex justify-between items-center mb-8 w-full">
       <AdjustButton
-        onClick={adjustTime}
+        onClick={handleAdjustTime}
         disabled={isActive}
         label="Session length"
         isSession={true}
         currentTime={workTime}
       />
       <AdjustButton
-        onClick={adjustTime}
+        onClick={handleAdjustTime}
         disabled={isActive}
         label="Break length"
         isSession={false}
