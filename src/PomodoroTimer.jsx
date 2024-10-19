e7520effc6c3a494574e951287c2ce86396ed332
Marcelo-G21 <marcelo.garcia.arenas@gmail.com>
2024-10-19 18:38:14 -0300

    feat: circles, gif and timer added

--- src/PomodoroTimer.jsx
+++ src/PomodoroTimer.jsx
@@ -1,20 +1,25 @@
-import TimerDisplay from "./components/TimerDisplay";
+import TimerDisplay from './components/TimerDisplay';
+import workGif from './assets/bunny-work.gif';
+import breakGif from './assets/bunny-break.gif';
 
 const PomodoroTimer = () => {
+  const time = '25:00';
+
   return (
-   <div>
-    <div className="title-container w-full mt-8">
-      <div className="flex items-center justify-center w-full">
-        <div className="flex-grow h-2 bg-indigo-400 md:h-2"></div>
-        <h1 className="text-4xl text-gray-800 mx-4 text-center">
-          Pomodoro Timer
-        </h1>
-        <div className="flex-grow h-2 bg-indigo-400 md:h-2"></div>
+    <div className="flex flex-col min-h-screen bg-indigo-100">
+      <div className="title-container w-full mt-8">
+        <div className="flex items-center justify-center w-full">
+          <div className="flex-grow h-2 bg-indigo-400 md:h-2"></div>
+          <h1 className="text-4xl text-gray-800 mx-4 text-center">
+            Pomodoro Timer
+          </h1>
+          <div className="flex-grow h-2 bg-indigo-400 md:h-2"></div>
+        </div>
+      </div>
+        <div className="flex flex-col p-6 sm:p-8 items-center w-full pl-12 pr-12">
+          <TimerDisplay workGif={workGif} breakGif={breakGif} time={time} />
       </div>
     </div>
-    <TimerDisplay/>
-   </div>
-    
   );
 };
 
--- src/components/TimerDisplay.jsx
+++ src/components/TimerDisplay.jsx
@@ -1,6 +1,46 @@
-const TimerDisplay = () => {
+const TimerDisplay = ({ time, workGif, breakGif }) => {
   return (
-    <div>TimerDisplay</div>
+    <div className="relative w-full max-w-xs md:max-w-80 md:-mt-20 mb-8 aspect-w-1 aspect-h-1">
+      <svg className="w-full h-full" viewBox="0 0 100 100">
+        <circle
+          className="text-indigo-300"
+          strokeWidth="4"
+          stroke="currentColor"
+          fill="transparent"
+          r="48"
+          cx="50"
+          cy="50"
+        />
+        <circle
+          className="text-indigo-500"
+          strokeWidth="4"
+          stroke="currentColor"
+          fill="transparent"
+          r="48"
+          cx="50"
+          cy="50"
+        />
+      </svg>
+
+      <div className="absolute inset-0 flex items-center justify-center z-10">
+        <img
+          src={workGif}
+          className="rounded-full w-40 h-40"
+          width={160}
+          height={160}
+          style={{ top: '-26px', position: 'relative' }}
+        />
+      </div>
+
+      <div className="absolute inset-0 flex items-center justify-center z-20">
+        <span
+          className="text-4xl font-bold text-gray-800 relative"
+          style={{ top: '60px' }}
+        >
+          {time}
+        </span>
+      </div>
+    </div>
   )
 }
 
