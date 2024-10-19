fa4ef65261c3e50a71f52fa977958fc16d2de3ac
Marcelo-G21 <marcelo.garcia.arenas@gmail.com>
2024-10-19 19:55:38 -0300

    feat: logic and props added to make work the component

--- src/PomodoroTimer.jsx
+++ src/PomodoroTimer.jsx
@@ -1,9 +1,46 @@
+import { useState, useEffect, useRef } from 'react';
 import TimerDisplay from './components/TimerDisplay';
 import workGif from './assets/bunny-work.gif';
 import breakGif from './assets/bunny-break.gif';
 
 const PomodoroTimer = () => {
-  const time = '25:00';
+  const [isWorking, setIsWorking] = useState(true);
+  const [time, setTime] = useState(25 * 60);
+  const [workTime, setWorkTime] = useState(25);
+  const [breakTime, setBreakTime] = useState(5);
+
+  useEffect(() => {
+    let interval = null;
+
+    if (time > 0) {
+      interval = setInterval(() => {
+        setTime(time => time - 1);
+      }, 1000);
+    } else if (time === 0) {
+      const newIsWorking = !isWorking;
+      setIsWorking(newIsWorking);
+      setTime(newIsWorking ? workTime * 60 : breakTime * 60);
+    } else {
+      if (interval) clearInterval(interval);
+    }
+
+    return () => {
+      if (interval) clearInterval(interval);
+    };
+  }, [time, isWorking, workTime, breakTime]);
+
+  const formatTime = timeInSeconds => {
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = timeInSeconds % 60;
+    return `${minutes.toString().padStart(2, '0')}:${seconds
+      .toString()
+      .padStart(2, '0')}`;
+  };
+
+  const calculateProgress = () => {
+    const totalTime = isWorking ? workTime * 60 : breakTime * 60;
+    return ((totalTime - time) / totalTime) * 100;
+  };
 
   return (
     <div className="flex flex-col min-h-screen bg-indigo-100">
@@ -16,8 +53,15 @@ const PomodoroTimer = () => {
           <div className="flex-grow h-2 bg-indigo-400 md:h-2"></div>
         </div>
       </div>
-        <div className="flex flex-col p-6 sm:p-8 items-center w-full pl-12 pr-12">
-          <TimerDisplay workGif={workGif} breakGif={breakGif} time={time} />
+      <div className="flex flex-col p-6 sm:p-8 items-center w-full pl-12 pr-12">
+        <TimerDisplay
+          isWorking={isWorking}
+          workGif={workGif}
+          breakGif={breakGif}
+          time={time}
+          formatTime={formatTime}
+          calculateProgress={calculateProgress}
+        />
       </div>
     </div>
   );
