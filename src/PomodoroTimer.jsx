0e4d4901aa1975ab0e887e525de58b2096eebe39
Marcelo-G21 <marcelo.garcia.arenas@gmail.com>
2024-10-19 17:49:06 -0300

    feat: added h1 and div lines

--- src/PomodoroTimer.jsx
+++ src/PomodoroTimer.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-
 const PomodoroTimer = () => {
   return (
-    <div>PomodoroTimer</div>
-  )
-}
+    <div className="title-container w-full mt-8">
+      <div className="flex items-center justify-center w-full">
+        <div className="flex-grow h-2 bg-indigo-400 md:h-2"></div>
+        <h1 className="text-4xl text-gray-800 mx-4 text-center">
+          Pomodoro Timer
+        </h1>
+        <div className="flex-grow h-2 bg-indigo-400 md:h-2"></div>
+      </div>
+    </div>
+  );
+};
 
-export default PomodoroTimer
+export default PomodoroTimer;
